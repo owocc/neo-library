6ff1bb191f6d320a8a9470e87431908d2b7505bc
小吉君 owo <owocc@qq.com>
2023-11-23 13:34:03 +0000

    swiper

--- components/Client/HomeSection/01.vue
+++ components/Client/HomeSection/01.vue
@@ -7,7 +7,7 @@
           Select E-Books
         </h1>
         <p class="mb-8">
-          Find the best e-books from your favor·ite writers, explore hundreds of
+          Find the best e-books from your favorite writers, explore hundreds of
           books with all possible categories, take advantage of the 50% discount
           and much more.
         </p>
@@ -18,7 +18,7 @@
         <!-- swiper -->
         <div class="grid">
           <!-- swiper item -->
-          <Swiper :loop='true'>
+          <Swiper :loop='true' class="bg-red-500">
 
             <SwiperSlide v-for="item in 4" class="w-[220px] transition-transform duration-400 scale-80">
               <img :src="`/img/home-book-${item + 1}.png`" alt="" class="w-[220px] transition-transform duration-400" />
